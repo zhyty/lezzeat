b37fbf67fd3b1ae177cbab5ac87dab16ac2be8e2
zhyty <tomyang711@gmail.com>
2016-09-17 15:21:39 -0400

    Fix restaurant table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160917183620) do
+ActiveRecord::Schema.define(version: 20160917191542) do
 
   create_table "groups", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160917183620) do
   end
 
   create_table "restaurants", force: :cascade do |t|
+    t.integer "group_id"
     t.string  "name"
     t.integer "rating"
     t.time    "start_hour"
