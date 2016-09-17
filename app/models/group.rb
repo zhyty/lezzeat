5a477ce8698c3ffc43d7ae3f77b74ebb57efb7e9
zhyty <tomyang711@gmail.com>
2016-09-17 15:09:11 -0400

    Add unique code to groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,12 @@ class Group < ActiveRecord::Base
   # users, id, restaurants
   has_many :users
   has_many :restaurants
+  after_initialize :init
 
-  validates_presence_of :location, :radius, :code
+  validates_presence_of :location, :radius
+  validates :radius, numericality: {only_integer: true}
+
+  def init
+    self.code ||= Code::generate_unique_code
+  end
 end
--- app/models/group/code.rb
+++ app/models/group/code.rb
@@ -1,9 +1,16 @@
-class Group::Code
-  def self.check_unique(code)
-    Group.find_by(code: code)
+module Group::Code
+  LENGTH = 4
+
+  module_function
+
+  def self.exist?(code)
+    !!Group.find_by_code(code)
   end
 
   def self.generate_unique_code
-
+    loop do
+      possible_code = ('a'..'z').to_a.shuffle[0, LENGTH].join
+      return possible_code unless exist?(possible_code)
+    end
   end
 end
