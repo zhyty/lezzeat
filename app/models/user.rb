c2891e06f1379c4247993165f090090237ab053e
zhyty <tomyang711@gmail.com>
2016-09-17 23:55:11 -0400

    Add user_vote incrementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
   # id, group_code, submitted
   belongs_to :group
+
+  # choices should be an array of ids
+  def submit_choices(choices)
+    return if self.submitted
+
+    choices.each do |id|
+      # FIXME may throw
+      Restaurant.increment_counter(:user_votes, id)
+    end
+
+    self.submitted = true
+  end
 end
